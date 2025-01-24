5f63afaf9b0275f4a51564a8daab2e2b13272b71
jayounglee92 <lzy6853@gmail.com>
2025-01-25 00:11:15 +0900

    스토리북 수정

--- src/stories/SearchBox/SearchBox.stories.ts
+++ src/stories/SearchBox/SearchBox.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import SearchBox from "./SearchBox";
+import SearchBox from "../../components/SearchBox/SearchBox";
 
 const meta = {
 	title: "Components/SearchBox",
