6f8a40929d486baa68caa443e374f699c0fe7559
jayounglee92 <lzy6853@gmail.com>
2025-02-12 17:14:06 +0900

    chore: cd.yml 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { worker } from "./mocks/browser";
 
 async function enableMocking() {
-	if (process.env.NODE_ENV !== "development") {
-		return;
+	if (
+		import.meta.env.MODE === "development" ||
+		import.meta.env.VITE_USE_MOCKS === "true"
+	) {
+		worker.start();
 	}
-
-	const { worker } = await import("./mocks/browser");
-	return worker.start();
+	return;
 }
 
 enableMocking().then(() => {
